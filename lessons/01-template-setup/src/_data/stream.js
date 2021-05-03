631baac25a12637a2361add368e291c901e194cb
5t3ph <thinkdobecreate@gmail.com>
2021-05-03 09:33:39 -0500

    01 Complete

---
+++ lessons/01-template-setup/src/_data/stream.js
@@ -0,0 +1,7 @@
+module.exports = {
+  twitchUsername: "5t3phDev",
+  title: "✨ Stream title here",
+  description: "@5t3ph // Stephanie Eckles",
+  cta: "Visit my site to sponsor me",
+  chatCommands: ["dev", "yay"]
+};
