8cf2ca7447f24c7f47c08d7e67a6cd31a36cb00c
5t3ph <thinkdobecreate@gmail.com>
2021-05-07 10:33:40 -0500

    04 Completed

--- lessons/04-chat-commands/.eleventy.js
+++ lessons/04-chat-commands/.eleventy.js
@@ -2,6 +2,10 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/css/");
   eleventyConfig.addPassthroughCopy("./src/css/");
 
+  eleventyConfig.addFilter("printJs", (js) => {
+    return JSON.stringify(js);
+  });
+
   return {
     dir: {
       input: "src",
--- lessons/04-chat-commands/src/_includes/comfyjs/script.js
+++ lessons/04-chat-commands/src/_includes/comfyjs/script.js
@@ -1,11 +1,28 @@
 const chatter = document.getElementById("chatter");
 const msgTemplate = document.getElementById("chatMsg");
+const main = document.querySelector("main");
 
 ComfyJS.onChat = (user, message) => {
   const newMsg = msgTemplate.content.cloneNode(true);
   newMsg.querySelector("strong").textContent = user;
   newMsg.querySelector("span").textContent = message;
   chatter.appendChild(newMsg);
+};
+
+ComfyJS.onCommand = (_user, command) => {
+  if (chatCommands.includes(command)) {
+    const originalNode = document.querySelector(`.command-${command}`);
+    const commandNode = originalNode.cloneNode(true);
+    main.appendChild(commandNode);
+
+    setTimeout(() => {
+      commandNode.classList.add("active");
+    }, 50);
+
+    setTimeout(() => {
+      commandNode.classList.remove("active");
+    }, 1500);
+  }
 };
 
 ComfyJS.Init(twitchUsername);
